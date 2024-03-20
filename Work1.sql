e3eb091e1208e66c522bc9c7eb95b095cba52185
Göksu Ayaz <goksuayaz79@gmail.com>
2024-03-21 01:43:35 +0300

    Work1

---
+++ Work1.sql
@@ -0,0 +1,16 @@
+-- Film:
+
+--SELECT title, description FROM film ;
+
+--SELECT * FROM film WHERE length > 60 AND length < 75;
+
+--SELECT * FROM film WHERE rental_rate  = 0.99 AND replacement_cost = 12.99 OR rental_rate = 28.99;
+
+--SELECT * FROM film WHERE length <= 50 AND rental_rate NOT IN (2.99, 4.99);
+
+-- Customer:
+
+--SELECT last_name FROM customer WHERE first_name = 'Mary';
+
+
+
